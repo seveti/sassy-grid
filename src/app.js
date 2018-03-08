6f181c987c1950a03a09aac5665047f3d526633b
Justin Runes <jrunes@viscira.com>
2018-03-07 17:46:04 -0800

    Updated new components

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 // require('./js/helloworld');
 // require('./js/test');
 import './main';
+
+// var img = new Image();   // Create new img element
+// img.src = '/assets/images/hcp/interior-1-1/desktop/1-1-interior-6-hcp.png';
+
+// img.onload = function() {
+ 
+//     document.body.appendChild(img);
+   
+// };
