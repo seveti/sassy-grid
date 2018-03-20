981de7316d62c43f5b50cb6681a253cd411047c0
Justin Runes <justin@seveti.com>
2018-03-19 21:06:23 -0700

    Reduced sassy-grid

--- src/js/helloworld.js
+++
@@ -1,9 +0,0 @@
-// Create Hello World node
-var div = document.createElement('div');
-var text = document.createTextNode('Hello World');
-
-// Append to text to div
-div.appendChild(text);
-
-// Append div to body
-// document.body.appendChild(div);
